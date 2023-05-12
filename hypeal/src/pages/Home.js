e937b3aa47f0ef762d484a05bad29b29a1dcb052
akshaygu <akshay180294@gmail.com>
2023-05-12 18:45:51 -0400

    feat: add first hypeal section to home page and add css settings

--- hypeal/src/pages/Home.js
+++ hypeal/src/pages/Home.js
@@ -1,24 +1,15 @@
-import logo from '../logo.svg';
+import React from "react";
+import HomeSection1 from "../components/HomeSection1.webp"
 
-const Home = () => {
+function Home() {
+
   return (
-    <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </>
+    <div className="Home">
+      <div className="Section">
+        <img style={{width:"100vw"}} src={HomeSection1}/>
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
