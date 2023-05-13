4e09fc74af1324f7963ccbdbbd3b46e6d27c942e
akshaygu <akshay180294@gmail.com>
2023-05-12 23:33:13 -0400

    feat: move first section into its own page

--- hypeal/src/pages/Home.js
+++ hypeal/src/pages/Home.js
@@ -11,34 +11,13 @@ import HomeVision from "./HomeVision"
 import HomeMetrics from "./HomeMetrics";
 import HomePartners from "./HomePartners";
 import HomeFooter from "./HomeFooter";
+import HomeMain from "./HomeMain";
 
 function Home() {
 
   return (
     <div className="Home">
-      <div className="Section">
-        <Grid container width={"60vw"} spacing={2} alignItems="flex-start">
-          <Grid item xs={12}>
-            <Typography color={"white"} variant="h6">Don't miss the Hypeal.
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography color={"white"} variant="body1">Join our Demo Day on March 15th, 2024.
-            </Typography>
-          </Grid>
-          <Grid item container xs={12} spacing={2} alignItems="center">
-            <Grid item xs={8}>
-              <HInput/>
-            </Grid>
-            <Grid item xs={4}>
-              <HButton variant="contained" color="primary" fullWidth>
-                KEEP ME POSTED
-              </HButton>
-            </Grid>
-          </Grid>
-        </Grid>
-        <img className="Background" src={HomeSection1}/>
-      </div>
+      <HomeMain/>
       <HomeCards/>
       <HomeInvest/>
       <HomeLaunch/>
